99c18e670e1cf779fb21850f53c8564f039ec088
selfishAltruism <officialkyus@gmail.com>
2024-04-01 14:05:43 +0900

    chore: sign in little design, connect router

--- src/page/auth/signIn/SignInPage.tsx
+++ src/page/auth/signIn/SignInPage.tsx
@@ -1,5 +1,18 @@
+import styled from "@emotion/styled/macro";
+
 const SignInPage = () => {
   return <></>;
 };
+
+const SignInWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainImg = styled.img`
+  width: 500px;
+`;
 
 export default SignInPage;
