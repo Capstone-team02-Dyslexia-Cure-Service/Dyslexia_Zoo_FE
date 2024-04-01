442f9bcaac7d4128baff8b5d91cd8afe7e715706
selfishAltruism <officialkyus@gmail.com>
2024-04-01 16:01:31 +0900

    feat: make sign in page

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -1,15 +1,16 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter as RootRouter, Routes, Route } from "react-router-dom";
 
-import AuthPageRouter from "./page/auth/AuthPageRoutes";
 import Loading from "./component/Loading";
 import { PAGE_URL } from "./config/path";
+
+const SignIn = lazy(() => import("./page/auth/signIn/SignInPage"));
 
 const PageRouter = () => (
   <Suspense fallback={<Loading />}>
     <RootRouter>
       <Routes>
-        <Route path={PAGE_URL.Auth} element={<AuthPageRouter />} />
+        <Route path={PAGE_URL.SignIn} element={<SignIn />} />
       </Routes>
     </RootRouter>
   </Suspense>
--- src/page/auth/signIn/SignInPage.tsx
+++ src/page/auth/signIn/SignInPage.tsx
@@ -1,10 +1,45 @@
 import styled from "@emotion/styled/macro";
+import { Controller, useForm } from "react-hook-form";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const SignInPage = () => {
-  return <></>;
+  const { control, handleSubmit } = useForm<User.SignInReqDto>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = () => {};
+  return (
+    <>
+      <SignInForm onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <TextField type="email" placeholder="이메일" {...field} />
+          )}
+        />
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <TextField type="password" placeholder="qlalfqjsgh" {...field} />
+          )}
+        />
+        <Button type="submit" variant="contained">
+          로그인
+        </Button>
+      </SignInForm>
+      <MainImg src="/img/signin_logo.png" alt="signin_logo" />
+    </>
+  );
 };
 
-const SignInWrapper = styled.div`
+const SignInForm = styled.form`
   display: flex;
   flex-direction: row;
   justify-content: center;
