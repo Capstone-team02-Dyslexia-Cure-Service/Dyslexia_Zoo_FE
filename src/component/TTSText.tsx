935627c6e3774d2f65ae8f469c2d37840e30cee7
selfishAltruism <officialkyus@gmail.com>
2024-04-08 16:05:58 +0900

    feat: complite TTSTesxt component

--- src/component/TTSText.tsx
+++ src/component/TTSText.tsx
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled/macro";
+import { useCallback } from "react";
 
-const TTSText = () => {
-  const getSpeech = (text) => {
-    let voices = [];
+const TTSText = (text: string) => {
+  const speech = useCallback((text: string) => {
+    let voices: SpeechSynthesisVoice[] = [];
 
     //디바이스에 내장된 voice를 가져온다.
     const setVoiceList = () => {
@@ -12,34 +13,40 @@ const TTSText = () => {
     setVoiceList();
 
     if (window.speechSynthesis.onvoiceschanged !== undefined) {
-      //voice list에 변경됐을때, voice를 다시 가져온다.
       window.speechSynthesis.onvoiceschanged = setVoiceList;
     }
 
-    const speech = (txt) => {
+    const speech = (text: string) => {
       const lang = "ko-KR";
-      const utterThis = new SpeechSynthesisUtterance(txt);
+      const utterThis = new SpeechSynthesisUtterance(text);
 
       utterThis.lang = lang;
 
-      /* 한국어 vocie 찾기
-               디바이스 별로 한국어는 ko-KR 또는 ko_KR로 voice가 정의되어 있다.
-            */
       const kor_voice = voices.find(
         (elem) => elem.lang === lang || elem.lang === lang.replace("-", "_")
       );
 
-      //힌국어 voice가 있다면 ? utterance에 목소리를 설정한다 : 리턴하여 목소리가 나오지 않도록 한다.
       if (kor_voice) {
         utterThis.voice = kor_voice;
       } else {
         return;
       }
 
-      //utterance를 재생(speak)한다.
       window.speechSynthesis.speak(utterThis);
     };
 
     speech(text);
+  }, []);
+
+  const clickHandler = () => {
+    speech(text);
   };
+
+  return <Text onClick={clickHandler}>{text}</Text>;
 };
+
+const Text = styled.div`
+  size: 20px;
+`;
+
+export default TTSText;
