f3806fa3e6ac254e60e9b87339fa7ad8161cbf36
selfishAltruism <officialkyus@gmail.com>
2024-04-08 15:44:12 +0900

    chore: change authservice name 2

---
+++ src/service/AuthService.tsx
@@ -0,0 +1,23 @@
+import { AxiosResponse } from "axios";
+
+import { API, setAccess, storeRefresh } from "@/config/axios";
+
+const AuthService = () => {
+  const URL = "api/v1/user";
+
+  const signin = async (body: User.SignInReqDto) => {
+    const {
+      data: { accessToken, refreshToken },
+    } = (await API.post(
+      `${URL}/sign-in`,
+      body
+    )) as AxiosResponse<User.SignInResDto>;
+
+    setAccess(accessToken);
+    storeRefresh(refreshToken);
+  };
+
+  return [signin];
+};
+
+export default AuthService;
