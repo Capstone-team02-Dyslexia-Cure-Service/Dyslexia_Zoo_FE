d47d7ae0082c8c4a1ff3fee4ac356461dcbeddf1
selfishAltruism <officialkyus@gmail.com>
2024-04-25 15:58:39 +0900

    chore: chage question file structure

---
+++ src/component/Container.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const QuestionContainer = styled(Container)`
+  flex-direction: row;
+
+  width: 800px;
+  height: 270px;
+
+  border-radius: 10px;
+
+  background-color: red;
+`;
--- src/component/question/WriteWordQuestion.tsx
+++ src/component/question/WriteWordQuestion.tsx
@@ -1,7 +1,14 @@
+import styled from "@emotion/styled";
 import TTSText from "@/component/TTSText";
 
+import { QuestionContainer } from "../Container";
+
 const WriteWordQuestion = ({ content }: { content: string }) => {
-  return <TTSText text={content} />;
+  return (
+    <QuestionContainer>
+      <TTSText text={content} />
+    </QuestionContainer>
+  );
 };
 
 export default WriteWordQuestion;
