7ee46fdf866bf407b4d0ac50cde9708b3e2ae107
selfishAltruism <officialkyus@gmail.com>
2024-04-25 16:51:57 +0900

    feat: disunite speech function

---
+++ src/component/function/speech.ts
@@ -0,0 +1,35 @@
+export const speech = (text: string) => {
+  let voices: SpeechSynthesisVoice[] = [];
+
+  //디바이스에 내장된 voice를 가져온다.
+  const setVoiceList = () => {
+    voices = window.speechSynthesis.getVoices();
+  };
+
+  setVoiceList();
+
+  if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = setVoiceList;
+  }
+
+  const speech = (text: string) => {
+    const lang = "ko-KR";
+    const utterThis = new SpeechSynthesisUtterance(text);
+
+    utterThis.lang = lang;
+
+    const kor_voice = voices.find(
+      (elem) => elem.lang === lang || elem.lang === lang.replace("-", "_")
+    );
+
+    if (kor_voice) {
+      utterThis.voice = kor_voice;
+    } else {
+      return;
+    }
+
+    window.speechSynthesis.speak(utterThis);
+  };
+
+  speech(text);
+};
