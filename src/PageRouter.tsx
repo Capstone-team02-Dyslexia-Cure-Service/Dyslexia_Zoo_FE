69c04a0636cc56ce8d22b0b436347bb966f95a16
selfishAltruism <officialkyus@gmail.com>
2024-04-30 14:20:34 +0900

    feat: create home page connect router

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -6,6 +6,7 @@ import { PAGE_URL } from "./config/path";
 
 const SignIn = lazy(() => import("./page/auth/signIn/SignInPage"));
 const BasicTest = lazy(() => import("./page/auth/basicTest/BasicTestPage"));
+const Home = lazy(() => import("./page/home/HomePage"));
 
 const PageRouter = () => (
   <Suspense fallback={<Loading />}>
@@ -13,6 +14,7 @@ const PageRouter = () => (
       <Routes>
         <Route path={PAGE_URL.SignIn} element={<SignIn />} />
         <Route path={PAGE_URL.BasicTest} element={<BasicTest />} />
+        <Route path={PAGE_URL.Home} element={<Home />} />
       </Routes>
     </RootRouter>
   </Suspense>
---
+++ src/page/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+import styled from "@emotion/styled";
+
+import { Background } from "@/component/Background";
+import TTSText from "@/component/TTSText";
+
+const BasicTestPage = () => {
+  return (
+    <>
+      <Background src="/img/home_background.png" alt="background" />
+    </>
+  );
+};
+
+export default BasicTestPage;
